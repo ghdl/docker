e3a5402043f1a349951249abd27a3a9dce72ddd0
1138-4EB <1138-4EB@users.noreply.github.com>
2019-02-25 06:47:55 +0100

    merge ansi_color.sh into utils.sh

--- travis/utils.sh
+++ travis/utils.sh
@@ -1,7 +1,20 @@
 #! /bin/sh
 
-. "$scriptdir/travis/ansi_color.sh"
-#disable_color
+enable_color() {
+  ENABLECOLOR='-c '
+  ANSI_RED="\033[31m"
+  ANSI_GREEN="\033[32m"
+  ANSI_YELLOW="\033[33m"
+  ANSI_BLUE="\033[34m"
+  ANSI_MAGENTA="\033[35m"
+  ANSI_CYAN="\033[36;1m"
+  ANSI_DARKCYAN="\033[36m"
+  ANSI_NOCOLOR="\033[0m"
+}
+
+disable_color() { unset ENABLECOLOR ANSI_RED ANSI_GREEN ANSI_YELLOW ANSI_BLUE ANSI_MAGENTA ANSI_CYAN ANSI_DARKCYAN ANSI_NOCOLOR; }
+
+enable_color
 
 # This is a trimmed down copy of https://github.com/travis-ci/travis-build/blob/master/lib/travis/build/bash/*
 travis_time_start() {
