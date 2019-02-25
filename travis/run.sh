f56c321c616d9b01f68ddaf4b4dd790e11e8b8a6
1138-4EB <1138-4EB@users.noreply.github.com>
2019-02-25 07:03:59 +0100

    merge build_test_pkg.sh into run.sh

--- travis/run.sh
+++ travis/run.sh
@@ -63,11 +63,36 @@ deploy () {
 
 #--
 
+build_img_pkg() {
+    travis_start "build_scratch" "$ANSI_BLUE[DOCKER build] ghdl/pkg:${IMAGE_TAG}$ANSI_NOCOLOR"
+    cd tmp-img
+    docker build -t ghdl/ghdl:$IMAGE_TAG . -f-<<EOF
+FROM scratch
+COPY $PKG ./
+COPY BUILD_TOOLS ./
+EOF
+    cd .. && rm -rf tmp-img
+    travis_finish "build_scratch"
+}
+
+build () {
+  cd ghdl
+  . ./dist/travis/travis-ci.sh
+
+  if [ "$TRAVIS_OS_NAME" != "osx" ]; then
+    if [ -f test_ok ]; then
+      build_img_pkg
+    fi
+  fi
+  cd ..
+}
+
+#--
+
 case "$1" in
-  -c) create
-  ;;
-  -e) extended
-  ;;
+  -b) build    ;;
+  -c) create   ;;
+  -e) extended ;;
   *)
     deploy $@
 esac
