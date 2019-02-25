0df044829a87f6799d5a1b42c3fedb8aa2c9037b
1138-4EB <1138-4EB@users.noreply.github.com>
2019-02-25 11:01:35 +0100

    fix: label for fedora29 (gcc)

--- hrcp.sh
+++ hrcp.sh
@@ -14,7 +14,7 @@ fi
 mcodegpl="stretch buster"
 mcode="stretch buster ubuntu14 ubuntu16 ubuntu18 fedora28 fedora29"
 llvm="ubuntu14-3.8 ubuntu16-3.9 ubuntu18-5.0 fedora28 fedora29"
-gcc="stretch-5.5.0 buster-7.2.0 fedora28-8.1.0 fedora28-8.2.1"
+gcc="stretch-5.5.0 buster-7.2.0 fedora28-8.1.0 fedora29-8.2.0"
 
 for BRANCH in $@; do
   if [ "$(git branch | grep "$BRANCH")" = "" ]; then
