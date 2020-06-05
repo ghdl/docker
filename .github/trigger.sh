26c05588dcef3bd8be3840b70d4e801642f607bf
eine <eine@users.noreply.github.com>
2020-06-05 02:29:00 +0200

    fix dispatching
    * add GHDL_BOT
    * trigger vunit and synth once only
    * use eine/dispatcher

--- .github/trigger.sh
+++ .github/trigger.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env sh
 
+set -e
+
 if [ -z $1 ]; then
   echo "Even type cannot be empty!"
   exit 1
 fi
 
 curl -X POST https://api.github.com/repos/ghdl/docker/dispatches \
--H "Content-Type: application/json" \
--H 'Accept: application/vnd.github.everest-preview+json' \
+-H "Content-Type: application/json" -H 'Accept: application/vnd.github.everest-preview+json' \
 -H "Authorization: token ${GHDL_BOT_TOKEN}" \
---data '{"event_type": "$1"}'
+--data "{\"event_type\": \"$1\"}"
